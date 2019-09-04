7cd0eb87352ffede28c6f0910720999db5aa51d1
Manav Malik <manavmalik36@gmail.com>
2019-09-04 18:59:03 -0400

    created user-friendly input file for scene creation

--- src/executables/RaytraceTest.java
+++ src/executables/RaytraceTest.java
@@ -8,7 +8,7 @@ import components.*;
 
 public class RaytraceTest
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		ArrayList<Vector> light = new ArrayList<>();
 		light.add(new Vector((float)8.7, (float)8.9, (float)-9.8));
@@ -16,13 +16,6 @@ public class RaytraceTest
 		sphere.add(new Sphere(new Vector(0, 0, -8), 2, new Material(new Color(0, 257, 0), 14)));
 		Scene s = new Scene(sphere, light);
 		File outFile = new File("FILEPATH"); // FILL IN THIS LINE
-		try
-		{
-			s.raytrace(outFile);
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
+		s.raytrace(outFile);
 	}
 }
---
+++ src/executables/Raytracer.java
@@ -0,0 +1,45 @@
+package executables;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import components.*;
+
+public class Raytracer
+{
+	private static Scanner input = new Scanner(System.in);
+	
+	public static void main(String[] args) throws IOException
+	{
+		System.out.print("Output Filename: ");
+		String filename = input.nextLine();
+		ArrayList<Item> items = new ArrayList<>();
+		int numSpheres = inputInt("Number of Spheres: ", 1)[0];
+		for(int i = 0; i < numSpheres; i++)
+		{
+			int[] coor = inputInt("Coordinates of Sphere " + (i + 1) + " in X Y Z format: ", 3);
+			int radius = inputInt("Radius of Sphere " + (i + 1) + ": ", 1)[0];
+			int[] color = inputInt("Color of Sphere " + (i + 1) + " in R G B format: ", 3);
+			items.add(new Sphere(new Vector(coor[0], coor[1], coor[2]), radius, new Material(new Color(color[0], color[1], color[2]))));
+		}
+		ArrayList<Vector> lightsource = new ArrayList<>();
+		int[] lightPos = inputInt("Position of Lightsource in X Y Z format: ", 3);
+		lightsource.add(new Vector(lightPos[0], lightPos[1], lightPos[2]));
+		
+		Scene s = new Scene(items, lightsource);
+		s.raytrace(new File(filename));
+	}
+	
+	public static int[] inputInt(String question, int nums)
+	{
+		System.out.print(question);
+		int[] responses = new int[nums];
+		for(int i = 0; i < nums; i++)
+		{
+			responses[i] = input.nextInt();
+		}
+		return responses;
+	}
+}
